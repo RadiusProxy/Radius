9c06b4925b10d63f4d50fac003efcfa0d4072c80
Ryan Wilson <inbox@ryanwilson.space>
2024-09-23 14:28:48 -0400

    Make UV the default proxy

--- deps.sh
+++ deps.sh
@@ -5,3 +5,4 @@ $AERO_PATH="public/aero" # The directory where aero's files should be
 
 curl https://raw.githubusercontent.com/vortexdeveloperlabs/sdk/refs/heads/main/aeroHandleSimple.js -o public/aeroHandleSimple.js
 cp ./node_modules/aero-proxy/examples/swWithSwitcher.js public/sw.js
+sed -i 's/const defaultProxy = "aero";/const defaultProxy = "uv";/' public/sw.js
