42af80da9c1d6e24192507af062f0431e4fffd34
Ryan Wilson <inbox@ryanwilson.space>
2024-09-24 11:45:30 -0400

    Route AeroSandbox as well

--- src/app/aero/[aero]/route.ts
+++ src/app/aero/[aero]/route.ts
@@ -2,8 +2,8 @@ import fs from 'fs'
 import { notFound } from 'next/navigation'
 import { NextRequest } from 'next/server'
 
-export async function GET(_req: NextRequest, { params }: { params: { uv: string } }) {
-  const requestedFile = params.uv
+export async function GET(_req: NextRequest, { params }: { params: { aero: string } }) {
+  const requestedFile = params.aero
   if (requestedFile === 'aero.config.js') {
     const file = fs.readFileSync(process.cwd() + `/src/lib/aero/${requestedFile}`)
     const fileBlob = new Blob([file])
