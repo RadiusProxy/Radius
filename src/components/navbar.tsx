fbba6a601760a7e881433518c4ed3bd2185457bf
ansh <ansh@anshnk.dev>
2024-10-12 13:28:28 -0500

    Added 404 Page, Fixed settings appearance page, and redirected logo to /

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,6 +5,7 @@ import { usePathname } from 'next/navigation'
 import { Button } from './ui/button'
 import { useState } from 'react'
 import Sidebar from './sidebar'
+import Link from 'next/link' // Import the Link component
 
 export default function Navbar() {
   const [open, setOpen] = useState(false)
@@ -18,10 +19,11 @@ export default function Navbar() {
         <Button onClick={() => setOpen(true)} size="icon" variant="ghost">
           <Lucide.Menu className="h-7 w-7" />
         </Button>
-        <div className="flex items-center gap-2">
+        {/* Wrap the logo and text in a Link */}
+        <Link href="/" className="flex items-center gap-2">
           <Lucide.Radius className="h-8 w-8 rotate-180" />
           <h1 className="text-xl font-bold">Radius</h1>
-        </div>
+        </Link>
       </div>
 
       <Sidebar open={open} onOpenChange={setOpen} />
