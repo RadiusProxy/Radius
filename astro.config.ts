19e6beaec45cdb5a6955d97dc19c1c0363544815
MotorTruck1221 <motortruck1221@protonmail.com>
2025-03-17 23:32:14 -0600

    A bunch of shit

--- astro.config.ts
+++ astro.config.ts
@@ -1,3 +1,30 @@
 import { defineConfig } from 'astro/config';
+import type { Plugin } from 'vite';
+import wisp from "wisp-server-node";
+import node from '@astrojs/node';
+import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({});
+const viteWispServer = (): Plugin => {
+    return {
+        name: 'vite-wisp-server',
+        configureServer(server) {
+            server.httpServer?.on('upgrade', (req, socket, head) => {
+                console.log('3');
+                req.url.startsWith('/wisp') ? wisp.routeRequest(req, socket, head) : undefined 
+            })
+        }
+    }
+};
+
+export default defineConfig({
+    vite: {
+        plugins: [
+            tailwindcss(),
+            viteWispServer()
+        ]
+    },
+    output: 'server',
+    adapter: node({
+        mode: 'middleware'
+    })
+});
---
+++ server/index.ts
@@ -0,0 +1,29 @@
+import Fastify, { FastifyReply, FastifyRequest } from 'fastify';
+import fastifyMiddie from '@fastify/middie';
+import fastifyStatic from '@fastify/static';
+import { fileURLToPath } from 'node:url';
+
+//@ts-ignore this is created at runtime. No types associated w/it
+import { handler as astroHandler } from "../dist/server/entry.mjs";
+
+
+const app = Fastify({
+    logger: true,
+    ignoreDuplicateSlashes: true,
+    ignoreTrailingSlash: true,
+});
+
+await app.register(fastifyStatic, {
+    root: fileURLToPath(new URL('../dist/client', import.meta.url))
+});
+
+await app.register(fastifyMiddie);
+
+await app.use(astroHandler);
+
+const port = parseInt(process.env.PORT as string) || parseInt("8080");
+
+app.listen({ port: port, host: "0.0.0.0" }).then(async () => {
+    console.log(`Server listening on http://localhost:${port}/`);
+    console.log(`Server also listening on http://0.0.0.0:${port}/`);
+});
