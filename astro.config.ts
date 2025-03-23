a0baded584c6cb9e995a533627c7acb3278aeea8
MotorTruck1221 <motortruck1221@protonmail.com>
2025-03-22 20:01:39 -0600

    Start to impl proxy stuff

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,14 @@ import wisp from "wisp-server-node";
 import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
 import icon from "astro-icon";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import playformCompress from "@playform/compress";
+import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
+import { scramjetPath } from "@mercuryworkshop/scramjet";
+//@ts-expect-error No types
+import { epoxyPath } from "@mercuryworkshop/epoxy-transport";
+import { libcurlPath } from "@mercuryworkshop/libcurl-transport";
+import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
 
 const viteWispServer = (): Plugin => {
     return {
@@ -20,10 +28,28 @@ export default defineConfig({
     vite: {
         plugins: [
             tailwindcss(),
-            viteWispServer()
+            viteWispServer(),
+            viteStaticCopy({
+                targets: [
+                    { src: `${uvPath}/**/*`.replace(/\\/g, "/"), dest: "vu", overwrite: false },
+                    { src: `${scramjetPath}/**/*`.replace(/\\/g, "/"), dest: "marcs", overwrite: false },
+                    { src: `${baremuxPath}/**/*`.replace(/\\/g, "/"), dest: "erab", overwrite: false },
+                    { src: `${epoxyPath}/**/*`.replace(/\\/g, "/"), dest: "epoxy", overwrite: false },
+                    { src: `${libcurlPath}/**/*`.replace(/\\/g, "/"), dest: "libcurl", overwrite: false }
+                ]
+            })
         ]
     },
-    integrations: [icon()],
+    integrations: [
+        icon(),
+        playformCompress({
+            CSS: false,
+            HTML: true,
+            Image: true,
+            JavaScript: true,
+            SVG: true
+        })
+    ],
     output: 'server',
     adapter: node({
         mode: 'middleware'
