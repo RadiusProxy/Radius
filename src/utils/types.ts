8af7b073fab1caace3c80b1b0ec427f64071a8f5
MotorTruck1221 <motortruck1221@protonmail.com>
2025-04-07 21:22:04 -0600

    Dynamically generate themes, and search engines

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -4,4 +4,16 @@ interface SettingsProps extends Props {
     active: 'appearance' | 'credits' | 'links' | 'proxy';
 }
 
-export type { SettingsProps };
+type DropdownOptions = {
+    name: string;
+    value: string;
+    default?: boolean;
+};
+
+const SearchEngines: Record<string, string> = {
+    DuckDuckGo: "https://duckduckgo.com/?q=%s",
+    Google: "https://google.com/search?q=%s",
+    Bing: "https://bing.com/search?q=%s",
+};
+
+export { type SettingsProps, type DropdownOptions, SearchEngines };
